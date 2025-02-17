4fed381413f8edf005994e49831b8ffefad8e3e8
Begum Akkas <Begum@MacBook-Air-2.local>
2025-02-17 13:20:43 -0600

    updated index

--- index/parks_index.py
+++ index/parks_index.py
@@ -1,8 +1,19 @@
 import geopandas as gpd
 import json
 from shapely.geometry import Point
-from collections import namedtuple
+# from collections import namedtuple
+from typing import NamedTuple
 
+class ParkTuple(NamedTuple):
+    point: Point
+
+class HousingTuple(NamedTuple):
+    park_count_800: int
+    park_count_1500: int
+    park_rating: int
+    total_reviews: int
+
+### Create BASE_DIR for the filepaths below (use pathlib)
 
 ##############################
 # Load Data
@@ -11,13 +22,15 @@ from collections import namedtuple
 parks = gpd.read_file("test_park_data.geojson")
 ## import osm parks geojson file
 ## currently using test data for parks
+## official data:
+parks = gpd.read_file("../data/parks_polygons.geojson")
 
 # housing data
 housing = gpd.read_file("../data/housing.geojson")
 
 # yelp ratings
 with open("../data/yelp/yelp_cleaned.json", "r") as f:
-    yelp_ratings = json.load(f)
+    ratings = json.load(f)
 
 
 
@@ -25,22 +38,23 @@ with open("../data/yelp/yelp_cleaned.json", "r") as f:
 # Create housing dictionary with index values
 ##############################
 
-def match_park_ratings_point(polygon):
+def match_park_ratings_point():
     """
-    Match yelp ratings to parks.
+    Match yelp and google ratings to parks.
 
     Args:
         polygon: polygon object of a park
 
     Returns: park rating if found, None otherwise
     """
-    for park in yelp_ratings:
-        rating_point = Point(park["longitude"], park["latitude"])
-        
-        if polygon.contains(rating_point):
-            rating = park["rating"]
-        else:
-            rating = None 
+    for polygon in parks.geometry:
+        for park in ratings:
+            rating_point = Point(park["longitude"], park["latitude"])
+            
+            if polygon.contains(rating_point):
+                rating = park["rating"]
+            else:
+                rating = None 
             
     # consider removing the park from yelp_ratings after rating is found to optimize efficiency
     # may need to think about if multiple polygons will match to the same rating
@@ -53,6 +67,18 @@ def match_park_ratings_name(polygon):
     pass
 
 
+def park_walking_distance(distance_m, house_point):
+    # set buffer around housing unit
+    buffer_800m = house_point.buffer(distance_m) 
+
+    park_count = 0
+    # think about ways to optimize so that you don't check every park in chicago
+    for polygon in parks.geometry:
+        if buffer_800m.intersects(polygon):
+            park_count += 1
+
+    return park_count
+
 
 def create_index_values(housing, parks):
     """_summary_
@@ -62,27 +88,18 @@ def create_index_values(housing, parks):
         parks (geopandas dataframe): parks data
     """
     housing_dict = {}
-    # maybe we should've done this within a class? 
-    housing_tuple = namedtuple('housing_values', ['park_count_800', 'park_rating'])
-    
-    for point in housing.geometry: # geometry accesses Point object
-        buffer_800m = point.buffer(800) # 800 meters roughly 10 min walking distance
-        # we can move the buffer to a helper if needed 
-        # will add 1-2 more buffers
-        
-        park_count = 0
-        # think about ways to optimize so that you don't check every park in chicago
-        for polygon in parks.geometry:
-            if buffer_800m.intersects(polygon):
-                park_count += 1
-               
-            rating = match_park_ratings_point(polygon)
-            if rating is None:
-                # call function to fuzzy match on park name & update rating value
-                rating = match_park_ratings_name(polygon) # update rating with fuzzy matched park name
 
-        house_values = housing_tuple(park_count_800=park_count, park_rating=rating)
-        housing_dict[point] = house_values # should we give each unit an ID other than the point?
+    for point in housing.geometry: # geometry accesses Point object
+        count_800 = park_walking_distance(point, 800) # 800 meters roughly 10 min walking distance
+        count_1500 = park_walking_distance(point, 1500)
+
+        rating = match_park_ratings_point()
+        if rating is None:
+            # call function to fuzzy match on park name & update rating value
+            rating = match_park_ratings_name() # update rating with fuzzy matched park name
+
+        house_tuple = HousingTuple(park_count_800=count_800, park_rating=rating)
+        housing_dict[point] = house_tuple # should we give each unit an ID other than the point?
 
     return housing_dict
 
@@ -93,4 +110,5 @@ def create_index_values(housing, parks):
 # Calculate indexes for each unit
 ##############################
     
+# loop through dictionary and create new dictionary with index values (maybe another namedtuple?)
 
