e76495df049fa83075cf55b0bbb5661e995a4744
Begum Akkas <Begum@MacBook-Air-2.local>
2025-02-22 15:08:30 -0600

    building out index and testing matches

--- index/parks_index.py
+++ index/parks_index.py
@@ -23,19 +23,14 @@ class HousingTuple(NamedTuple):
 ##############################
 # Load Data -- might want to make these into functions 
 ##############################
-# parks data
-# parks = gpd.read_file("test_park_data.geojson")
-
-## import osm parks geojson file
-## currently using test data for parks
-## official data:
-parks = gpd.read_file("../data/parks_polygons.geojson")
+# load parks data
+parks = gpd.read_file("data/parks_polygons.geojson")
 
 # housing data
-housing = gpd.read_file("../data/housing.geojson")
+# housing = gpd.read_file("../data/housing.geojson")
 
-# yelp and google ratings
-with open("../data/yelp/yelp_cleaned.json", "r") as f:
+# # yelp and google ratings
+with open("data/yelp/yelp_cleaned.json", "r") as f:
     ratings = json.load(f)
 
 
@@ -56,6 +51,7 @@ def calculate_park_rating(matching_rows, polygon):
     """
     total_reviews = 0
     cumulative_rating = 0
+    avg_rating = 0
     park_name = None
 
     for row in matching_rows:
@@ -69,10 +65,10 @@ def calculate_park_rating(matching_rows, polygon):
         park_name = name  # Last matched name (assuming one park per polygon)
 
     if total_reviews == 0:
-        return None  # No ratings found
-
-    # Compute average rating
-    avg_rating = cumulative_rating / total_reviews
+        total_reviews = None  # No ratings found
+    else:
+        # Compute average rating
+        avg_rating = cumulative_rating / total_reviews
 
     return ParkTuple(park_polygon=polygon, name=park_name, 
                      rating=avg_rating, total_reviews=total_reviews, area=polygon.area)
@@ -89,7 +85,7 @@ def match_park_ratings_point(polygon):
         ParkTuple if found, None otherwise.
     """
     matching_rows = []
-    
+        
     for row in ratings:
         review_point = Point(row["longitude"], row["latitude"])
         
@@ -117,7 +113,7 @@ def match_park_ratings_name(park_name, polygon):
     for row in ratings:
         sim_score = jaro_winkler_similarity(row["name"], park_name)
         
-        if sim_score > 0.9:
+        if sim_score > 0.8:
             matching_rows.append(row)
     
     park_tuple = calculate_park_rating(matching_rows, polygon)
@@ -125,8 +121,7 @@ def match_park_ratings_name(park_name, polygon):
     return park_tuple
 
 
-
-def create_parks_dict():
+def create_parks_dict(parks):
     """_summary_
 
     Returns:
@@ -134,16 +129,49 @@ def create_parks_dict():
     """
     parks_dict = defaultdict(int)
     
-    for _, park in parks:
+    # buffer the polygon to test match rate
+    # to check current CRS: print(parks.crs) # (should be EPSG: 4326)
+    # convert to a matric CRS (EPSG to meters)
+    # parks = parks.to_crs(epsg=3857)
+    # # apply buffer in meters
+    # parks["buffered_polygon"] = parks.geometry.buffer(200)
+    # # convert back to lat/long
+    # parks = parks.to_crs(epsg=4236)
+    
+    parks_without_names = 0 # FOR DEBUGGING; DELETE LATER
+
+    for _, park in parks.iterrows():
         polygon = park.geometry
         park_name = park["name"]
+        ##### FOR DEBUGGING PURPOSES
+        if park_name is None:
+            parks_without_names += 1
+        
         park_tuple = match_park_ratings_point(polygon)
         
         # if reviews not found from point, use park name to match
-        if park_tuple.total_reviews is None:
+        if park_tuple.total_reviews is None and park_name is not None:
+            # print(park_name)
             park_tuple = match_park_ratings_name(park_name, polygon)
         
         parks_dict[polygon] = park_tuple
+        
+    #########################
+    ##### FOR DEBUGGING PURPOSES
+    none_count = 0
+    populated_count = 0
+    
+    for key, value in parks_dict.items():
+        if value.total_reviews is None:
+            none_count += 1
+        else:
+            populated_count += 1
+                  
+    print("parks with ratings:", populated_count)
+    print("parks withOUT ratings:", none_count)
+    
+    print("total nameless parks:", parks_without_names)
+    #########################
     
     return parks_dict
 
@@ -157,66 +185,66 @@ def create_parks_dict():
 ##############################
 
 
-def park_walking_distance(house_point, distance):
-    # set buffer around housing unit
-    buffer_800m = house_point.buffer(distance) 
-    polygon_list = []
-    park_count = 0
+# def park_walking_distance(house_point, distance):
+#     # set buffer around housing unit
+#     buffer_800m = house_point.buffer(distance) 
+#     polygon_list = []
+#     park_count = 0
     
-    # think about ways to optimize so that you don't check every park in chicago
-    for polygon in parks.geometry:
-        # we can also loop through parks dictionary instead to only consider parks with reviews?
-        if buffer_800m.intersects(polygon):
-            park_count += 1
-            polygon_list.append(polygon)
+#     # think about ways to optimize so that you don't check every park in chicago
+#     for polygon in parks.geometry:
+#         # we can also loop through parks dictionary instead to only consider parks with reviews?
+#         if buffer_800m.intersects(polygon):
+#             park_count += 1
+#             polygon_list.append(polygon)
 
-    return (park_count, polygon_list)
+#     return (park_count, polygon_list)
 
 
-def calculate_index(polygon_list, parks_dict):
-    size_rating_index = 0
+# def calculate_index(polygon_list, parks_dict):
+#     size_rating_index = 0
     
-    for polygon in polygon_list:
-        park_tuple = parks_dict[polygon]
-        size_rating_index += (park_tuple.area * park_tuple.rating)
+#     for polygon in polygon_list:
+#         park_tuple = parks_dict[polygon]
+#         size_rating_index += (park_tuple.area * park_tuple.rating)
         
-    return size_rating_index
+#     return size_rating_index
 
 
-def create_house_tuple(point, parks_dict, distance):
-    """_summary_
+# def create_house_tuple(point, parks_dict, distance):
+#     """_summary_
 
-    Args:
-        housing (geopandas dataframe): affordable housing data
-        parks (geopandas dataframe): parks data
-    """
-    parks_buffer_count, polygon_list = park_walking_distance(point, distance) 
-    # for distance (meters): 800 meters roughly 10 min walking distance
+#     Args:
+#         housing (geopandas dataframe): affordable housing data
+#         parks (geopandas dataframe): parks data
+#     """
+#     parks_buffer_count, polygon_list = park_walking_distance(point, distance) 
+#     # for distance (meters): 800 meters roughly 10 min walking distance
 
-    # gather park tuples that fall within radius
-    index = calculate_index(polygon_list, parks_dict)
+#     # gather park tuples that fall within radius
+#     index = calculate_index(polygon_list, parks_dict)
     
-    house_tuple = HousingTuple(park_count=parks_buffer_count, park_index=index)
-    # housing_dict[point] = house_tuple 
+#     house_tuple = HousingTuple(park_count=parks_buffer_count, park_index=index)
+#     # housing_dict[point] = house_tuple 
 
-    return house_tuple
+#     return house_tuple
 
 
 
-##############################
-# Create index values dictionary
-##############################
+# ##############################
+# # Create index values dictionary
+# ##############################
 
-def create_index_values(housing, parks_dict, distance):
-    ## Currently calling this function would create separate dictionaries for each distance/radius
-    ## should we change function to create one dictionary where the values are a list of house tuples?
-    housing_dict = {}
+# def create_index_values(housing, parks_dict, distance):
+#     ## Currently calling this function would create separate dictionaries for each distance/radius
+#     ## should we change function to create one dictionary where the values are a list of house tuples?
+#     housing_dict = {}
     
-    for point in housing.geometry: # geometry accesses Point object
-        house_tuple = create_house_tuple(point, parks_dict, distance)
+#     for point in housing.geometry: # geometry accesses Point object
+#         house_tuple = create_house_tuple(point, parks_dict, distance)
         
-        housing_dict[point] = house_tuple 
+#         housing_dict[point] = house_tuple 
         
-    return housing_dict
+#     return housing_dict
 
             
