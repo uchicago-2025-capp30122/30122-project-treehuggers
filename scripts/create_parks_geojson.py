ad600bd48209a83f6c1662a88c59bcfa6d3fba00
grace-kluender <graceek@uchicago.edu>
2025-02-23 11:53:28 -0600

    OSM scraping script & cleaning script functional

--- scripts/create_parks_geojson.py
+++ scripts/create_parks_geojson.py
@@ -33,19 +33,16 @@ parks_polygons = parks[parks.geometry.geom_type == "Polygon"]
 project_root = os.path.dirname(os.getcwd())
 # Define data folder path
 data_folder = os.path.join(project_root, "data")
-# Define points filepath
-points_filepath = os.path.join(data_folder, "parks_points.geojson")
+
 # Define polygons filepath
 polygons_filepath = os.path.join(data_folder, "parks_polygons.geojson")
 
-# Save to GeoJSON files
-parks_points[required_columns].to_file(points_filepath, driver="GeoJSON")
+# Save to GeoJSON file
 parks_polygons[required_columns].to_file(polygons_filepath, driver="GeoJSON")
 
 # Pretty-print the GeoJSON files
-for filename in [points_filepath, polygons_filepath]:
-    with open(filename, "r") as f:
-        data = json.load(f)
+with open(polygons_filepath, "r") as f:
+    data = json.load(f)
 
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
+with open(polygons_filepath, "w") as f:
+    json.dump(data, f, indent=4)
