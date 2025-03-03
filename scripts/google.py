589218960dec5db28245711713366f52aa0e607e
evanfantozzi <evanfantozzi@gmail.com>
2025-03-03 14:53:22 -0600

    Issues with pytest still

--- scripts/google.py
+++ scripts/google.py
@@ -15,8 +15,6 @@ except KeyError:
         "Please enter API Key for Google"
     )
     
-
-    
 def cached_get_google(url, kwargs: dict) -> dict:
     '''
     Fetches API data from Google based on inputted URL and arguments
@@ -39,27 +37,34 @@ def cached_get_google(url, kwargs: dict) -> dict:
         return all_data_dict
     
     # Else get from Google
+    kwargs["key"] = GOOGLE_API_KEY
     all_places = []
     
+    # Loop through list of 15 locations distributed throughout Chicago
     for loc in CHICAGO_LOCATIONS:
         next_page_token = None
         for i in range(3): # Limit of 60 results per search
             
+            # Set location argument equal to lat/lon coordinates in loop 
             kwargs["location"] = f"{loc[0]},{loc[1]}"
-            kwargs["key"] = GOOGLE_API_KEY
+            
+            # Set page token either to None or from previous httpx response
             kwargs["page_token"] = next_page_token
             
             response = httpx.get(url, params=kwargs)
             if response.status_code == 200:
+                # Extend data list with fetched results, set page token  
                 data = response.json()
                 all_places.extend(data.get("results", [])) 
                 print("Getting results", i, "for", loc)
                 next_page_token = data.get("next_page_token", None)
                 if not next_page_token: 
+                    # No more results
                     break
                 else:
                     time.sleep(10)
             else:
+                # Error fetching
                 raise FetchException(response)
             
     # Save in cache 
@@ -79,6 +84,7 @@ def clean_google(data: dict):
     '''
     places = []
     for place in data["places"]:
+        # Keep relevant information on park location/quality
         places.append(
             {
             "name": place.get("name", "N/A"),
@@ -102,8 +108,9 @@ def save_google(places: list[dict], output_name: str):
 if __name__ == "__main__":
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     
+    # Run various searches to be concatenated 
     for search_category in ["park", "field", "stadium"]:
-        parameters = {"radius": "3590"}
+        parameters = {"radius": "3590"} # Roughly dividing Chicago in 15 areas
     
         # Either search using "type" or using a keyword
         if search_category == "park":
@@ -114,3 +121,4 @@ if __name__ == "__main__":
         google_raw_data = cached_get_google(url, parameters)
         google_clean_data = clean_google(google_raw_data)
         save_google(google_clean_data, "google_"+search_category)
+
